15863706f89724acf636ea29408304456030e7fd
humberto <huarci@gmail.com>
2022-03-01 22:31:22 -0600

    Adding a handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("Ti");
+  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -10,6 +13,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
